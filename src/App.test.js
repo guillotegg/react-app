2a36d993bc85b6813218b2394b8a92d97a030c1b
MCasanova <mcasanova@primerosystems.com>
2019-09-11 12:04:22 -0300

    Add Unit Test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-test('should correctly render App', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper).toMatchSnapshot();
+describe('Grid', () => {
+  test('should correctly render App', () => {
+    const wrapper = shallow(<App />); 
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  let result;
+  test('should render a div when bound to null', () => {
+      result = shallow(<App data={null} />);
+      expect(result.type()).toEqual('div');
+  })
 });
